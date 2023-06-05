92496170acadf4b31b3e9c8c823414c3c2825d53
Tre <tregallowayofficial@gmail.com>
2023-06-05 16:30:48 -0500

    Setting up services page

---
+++ src/routes/services/+page.svelte
@@ -0,0 +1,38 @@
+<script></script>
+
+<div class=" md:mx-20 mx-4 my-4">
+	<div class="py-4"><h1 class="h1 text-center underline underline-offset-8">Services</h1></div>
+	<div>
+		<div class="grid grid-cols-1 md:grid-cols-2">
+			<div>
+				<h2 class="h2">Home Repair</h2>
+				<ul class="list-disc">
+					<li>Plumbing</li>
+					<li>Sheetrock</li>
+					<li>Carpenting</li>
+					<li>Electrical</li>
+					<li>Painting</li>
+					<li>Flooring</li>
+				</ul>
+			</div>
+			<div />
+		</div>
+		<div class="">
+			<h2 class="h2">Homecare</h2>
+			<ul class="list-disc">
+				<li>Lawncare</li>
+				<li>Pressure Washing</li>
+				<li>Housekeeping</li>
+				<li>Carpet Cleaning</li>
+				<li>Drapes & Blindes</li>
+			</ul>
+		</div>
+		<div class="">
+			<h2 class="h2">Events</h2>
+			<ul class="list-disc">
+				<li>Waiter Service</li>
+				<li>Event Setup & Takedown</li>
+			</ul>
+		</div>
+	</div>
+</div>
